7835bfdb1e728388191420570ec02f644fcfd9b0
yamilzavala <zavalayamil9@gmail.com>
2025-01-06 18:23:06 -0300

    netlify redirect added

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext();
+
 export const UserProvider = ({ children }) => {
   const [myUser, setMyUser] = useState(null);
   const {user, loginWithRedirect, logout} = useAuth0();
